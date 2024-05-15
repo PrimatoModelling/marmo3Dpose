34d7e489ce061846de81d2341abb472e695d37c3
KanekoTakaaki <104041914+KanekoTakaaki@users.noreply.github.com>
2024-05-15 22:34:48 +0900

    Updates

---
+++ src/m_lib/readme.txt
@@ -0,0 +1,2 @@
+This code is from mvpose (https://github.com/zju3dv/mvpose) 
+Part of code in "reconst_multi_step2.py" is also derived from mvpose. 
---
+++ src/m_lib/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import numpy
+
+ext_modules=[ Extension("pictorial",
+              ["pictorial.pyx"],
+              libraries=["m"],
+              extra_compile_args = ["-ffast-math", "-O3"])]
+
+setup(
+  name = "pictorial",
+  cmdclass = {"build_ext": build_ext},
+  ext_modules = ext_modules,
+  include_dirs = [numpy.get_include()])
